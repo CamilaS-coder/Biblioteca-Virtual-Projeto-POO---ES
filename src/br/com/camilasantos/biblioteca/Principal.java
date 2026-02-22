924e19bd0bfcc2ee83498afa8dfb6fdfcdc004ae
Camila Santos <camilasantoss1302@gmail.com>
2026-02-22 19:22:12 -0300

    feat: inicializar projeto Java e criar classe Principal

---
+++ src/br/com/camilasantos/biblioteca/Principal.java
@@ -0,0 +1,11 @@
+package br.com.camilasantos.biblioteca;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sistema Biblioteca iniciado com sucesso!");
+
+    }
+
+}
